8e9ee4ea3428a8ea1365cd0d713e6c005e272eda
Denis Khatnyuk <d.khatnyuk@gmail.com>
2024-03-07 00:33:53 +0000

    implemented initial idea of new monte carlo

--- MonteCarlo/monte_carlo.py
+++ MonteCarlo/monte_carlo.py
@@ -1,5 +1,6 @@
 # Montecarlo Simulation base code (No data collection or processing)
 import numpy as np
+from numpy.random import sample
 import pandas as pd
 
 
@@ -24,9 +25,9 @@ def montecarlo(price, strike, maturity, rfr, volatility, drift, timeSteps=50, si
     
     
     # if there was actual input data, dummy data for testing purposes
-    dates = pd.date_range(start="2022-01-01", end="2023-01-01", freq='B')
-    dummy_prices = strike * np.exp(np.cumsum(np.random.normal(0, volatility,
-    size=len(dates))))
+    # dates = pd.date_range(start="2022-01-01", end="2023-01-01", freq='B')
+    # dummy_prices = strike * np.exp(np.cumsum(np.random.normal(0, volatility,
+    # size=len(dates)))) -Don't need these dummy prices anymore.
     
     #historical_data = pd.DataFrame(data={'Date': dates, 'Price':dummy_prices}) 
     #historical_data.set_index('Date', inplace=True)
@@ -49,9 +50,45 @@ def montecarlo(price, strike, maturity, rfr, volatility, drift, timeSteps=50, si
     CO = (np.exp(-rfr * maturity) * np.sum(np.maximum(S[-1] - strike, 0))) / simulations
     return CO  
 
+def montecarlo_rolling(init_price, strike, rfr, sample_returns, timeSteps=30, simulations=5000):
+    num_samples = sample_returns.size
+    sample_returns = np.tile(sample_returns, simulations).reshape((num_samples, simulations))
+    #sample_returns must be given as a numpy array.
+    N = np.sqrt(252) # Conversion param to put variance into correct units.
+    dt = 1/252 #length of timeStep, in (trading) years. Using 1 trading day.
+
+    prices = np.zeros((timeSteps + 1, simulations)) # add one, as we keep original.
+    sim_returns = np.zeros((timeSteps,simulations))
+
+    prices[0] = init_price
+     
+    for time in range(timeSteps):
+        returns = np.concatenate((sample_returns, sim_returns[:time]),axis=0)
+        drift = returns.mean() * 252
+        volatility = returns.std() * N
+        z = np.random.standard_normal(simulations) # array of n standard normal samples.
+        prices[time + 1] = prices[time] * np.exp((drift - 0.5 * volatility ** 2) * dt + volatility * np.sqrt(dt) * z)
+        sim_returns[time] = (prices[time + 1] / prices[time]) - 1
+
+    avg_gain = (np.exp(-rfr * timeSteps/252) * np.sum(np.maximum(prices[-1] - strike, 0))) / simulations
+    
+    return avg_gain
+
+    # I won't allow manual maturity, as this code needs timesteps to be in days. Will calculate maturity based off time steps.
+    # A modification of the montecarlo code to update the volatility, drift based off the new time.
+    
+    # Similarly, need the sample to re-calculate volatility and spread.
+
+  
+
+
+
+
+
 if __name__ == "__main__":
-    result = montecarlo(100, 105, 1, 0.0388, 0.5)
-    print(result)
+    # result = montecarlo(100, 105, 1, 0.0388, 0.5)
+    # print(result)
+    pass
         
     
     
